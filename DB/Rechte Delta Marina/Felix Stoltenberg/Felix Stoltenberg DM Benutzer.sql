d5c141cf8e63ccfdd5335c0012b97c45c692430c
Feriixu <Feriixu@users.noreply.github.com>
2019-04-10 10:11:23 +0200

    Create Felix Stoltenberg DM Benutzer.sql

---
+++ DB/Rechte Delta Marina/Felix Stoltenberg/Felix Stoltenberg DM Benutzer.sql
@@ -0,0 +1,8 @@
+CREATE USER IF NOT EXISTS 'Service'@'localhost' IDENTIFIED BY 'supp0rt';
+GRANT ALL PRIVILEGES ON * . * TO 'newuser'@'localhost';
+
+CREATE USER IF NOT EXISTS 'Buchhaltung'@'localhost' IDENTIFIED BY 'buchh4ltung';
+GRANT ALL PRIVILEGES ON * . * TO 'newuser'@'localhost';
+
+CREATE USER IF NOT EXISTS 'Vertrieb'@'localhost' IDENTIFIED BY 'ver7rieb';
+GRANT ALL PRIVILEGES ON * . * TO 'newuser'@'localhost';
